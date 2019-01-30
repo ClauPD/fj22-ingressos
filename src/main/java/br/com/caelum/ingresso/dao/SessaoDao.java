c299d33e1d0f5c0ca654cf6aa0386d8e81461b80
Claudio Domingues <claudio.domingues68@hotmail.com>
2019-01-30 17:13:52 -0200

    Selecionando lugares

--- src/main/java/br/com/caelum/ingresso/dao/SessaoDao.java
+++ src/main/java/br/com/caelum/ingresso/dao/SessaoDao.java
@@ -38,6 +38,10 @@ public class SessaoDao {
 			.setParameter("filme", filme)
 			.getResultList();
 		}
+		
+		public Sessao findOne(Integer id) {
+			return manager.find(Sessao.class, id);
+		}
 }
 
 
